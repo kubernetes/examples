kubernetes/kubernetes

split insecure serving options

examples/apiserver/apiserver.go
@@ -55,8 +55,8 @@ func newStorageFactory() genericapiserver.StorageFactory {
 }
 
 func NewServerRunOptions() *genericoptions.ServerRunOptions {
-	serverOptions := genericoptions.NewServerRunOptions().WithEtcdOptions().WithSecureServingOptions()
-	serverOptions.InsecurePort = InsecurePort
+	serverOptions := genericoptions.NewServerRunOptions().WithEtcdOptions().WithSecureServingOptions().WithInsecureServingOptions()
+	serverOptions.InsecureServingOptions.BindPort = InsecurePort
 	return serverOptions
 }
 

kubernetes/examples

apiserver/apiserver.go
@@ -56,8 +56,8 @@ func newStorageFactory() genericapiserver.StorageFactory {
 }
 
 func NewServerRunOptions() *genericoptions.ServerRunOptions {
-	serverOptions := genericoptions.NewServerRunOptions().WithEtcdOptions().WithSecureServingOptions()
-	serverOptions.InsecurePort = InsecurePort
+	serverOptions := genericoptions.NewServerRunOptions().WithEtcdOptions().WithSecureServingOptions().WithInsecureServingOptions()
+	serverOptions.InsecureServing.BindPort = InsecurePort
 	return serverOptions
 }
 